85d6e189884a50df61d1954229f3da42f4759d89
Anderson Santos <andersonribeiroow@gmail.com>
2020-04-26 19:04:29 -0300

    Added basics of game

---
+++ Assets/Scripts/RacketPlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacketPlayerMovement : MonoBehaviour {
+
+	public float movementSpeed;
+    public string axis;
+
+    private void FixedUpdate()
+    {
+        float v = Input.GetAxisRaw(axis);
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * movementSpeed;
+    }
+}
